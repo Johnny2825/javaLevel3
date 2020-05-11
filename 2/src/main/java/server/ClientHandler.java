25eec78b3267e474bf7121f6c27df68e4fa82923
Johnny2825 <35284499+Johnny2825@users.noreply.github.com>
2020-05-11 11:36:26 +0900

    HM4

--- 2/src/main/java/server/ClientHandler.java
+++ 2/src/main/java/server/ClientHandler.java
@@ -4,36 +4,33 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.*;
+import java.util.concurrent.*;
 
 public class ClientHandler {
     private MyServer myServer;
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
-    private Timer timer;
-    private TimerTask task;
-
+    private ScheduledExecutorService executorService;
+    private Future timer;
     private String name;
 
-    public ClientHandler(MyServer myServer, Socket socket) {  //конструктор обработчика клиента, на входе сервер и сокет
+    public ClientHandler(MyServer myServer, Socket socket) {
         try {
             this.myServer = myServer;
             this.socket = socket;
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
             this.name = "";
+            executorService = Executors.newScheduledThreadPool(2);
 
-//            task = new TimerTask() {
-//                public synchronized void run() {
-//                    closeConnection();
-//                }
-//            };
-//            timer = new Timer("Timer");
-//            long delay = 5000L;
-//            timer.schedule(task, delay);
+            timer = executorService.schedule(() -> {
+                sendMsg("Время ожидания вышло");
+                closeConnection();
+                System.out.println("Соединение закрыто по времени");
+            }, 120, TimeUnit.SECONDS);
 
-            new Thread(() -> {
+            new Thread(() -> executorService.execute(() -> {
                 try {
                     authentication();
                     readMessages();
@@ -42,38 +39,43 @@ public class ClientHandler {
                 } finally {
                     closeConnection();
                 }
-            }).start();
+            }
+            )).start();
+
         } catch (IOException e) {
             throw new RuntimeException("Проблемы при создании обработчика клиента");
         }
     }
 
-
     public void authentication() throws IOException {
-
         while (true) {
-            String str = null;
-            str = in.readUTF();
-            if(str.startsWith("/end")){
-                closeConnection();
-            }
-            if (str.startsWith("/auth")) {
-                String[] parts = str.split("\\s");
-                String nick = myServer.getAuthService().getNickByLoginPass(parts[1], parts[2]);
-                if (nick != null) {
-                    if (!myServer.isNickBusy(nick)) {
-                        //timer.cancel();
-                        sendMsg("/authok " + nick);
-                        name = nick;
-                        myServer.broadcastMsg(name + " зашел в чат");
-                        myServer.subscribe(name, this);
-                        return;
-                    } else {
-                        sendMsg("Учетная запись уже используется");
-                    }
-                } else {
-                    sendMsg("Неверные логин/пароль");
+            String str;
+            System.out.println(socket.isClosed());
+            if (in != null) {
+                str = in.readUTF();
+                if (str.startsWith("/end")) {
+                    closeConnection();
                 }
+                if (str.startsWith("/auth")) {
+                    String[] parts = str.split("\\s");
+                    String nick = myServer.getAuthService().getNickByLoginPass(parts[1], parts[2]);
+                    if (nick != null) {
+                        if (!myServer.isNickBusy(nick)) {
+                            timer.cancel(true);
+                            sendMsg("/authok " + nick);
+                            name = nick;
+                            myServer.broadcastMsg(name + " зашел в чат");
+                            myServer.subscribe(name, this);
+                            return;
+                        } else {
+                            sendMsg("Учетная запись уже используется");
+                        }
+                    } else {
+                        sendMsg("Неверные логин/пароль");
+                    }
+                }
+            } else {
+                System.out.println("In is null");
             }
         }
     }
@@ -86,12 +88,12 @@ public class ClientHandler {
                 if (strFromClient.equals("/end")) {
                     return;
                 }
-                if (strFromClient.startsWith("/change nick")){      //смена ника
+                if (strFromClient.startsWith("/change nick")){
                     String[] tokens = strFromClient.split("\\s");
                     myServer.updateNick(name, tokens[2], this);
                     name = tokens[2];
                 }
-                if (strFromClient.startsWith("/w")){        //личная отсылка
+                if (strFromClient.startsWith("/w")){
                     String[] tokens = strFromClient.split("\\s");
                     myServer.unicastMsg(name, tokens[1], strFromClient.substring(6 + tokens[1].length()));
                     sendMsg("Personally to " + tokens[1] + ":" + strFromClient.substring(5 + tokens[1].length()));
@@ -111,9 +113,11 @@ public class ClientHandler {
         }
     }
 
-    public void closeConnection() {
-        myServer.unsubscribe(name);
-        myServer.broadcastMsg(name + " вышел из чата");
+    public void closeConnection(){
+        if (!name.isEmpty()){
+            myServer.unsubscribe(name);
+            myServer.broadcastMsg(name + " вышел из чата");
+        }
         try {
             in.close();
         } catch (IOException e) {
@@ -129,6 +133,7 @@ public class ClientHandler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        executorService.shutdown();
     }
 }
 
